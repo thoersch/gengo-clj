a7d3c4f09a7d63ffc7fb439c11e347167e78fb9b
Tyler Hoersch <thoersch@gmail.com>
2014-09-29 22:24:43 -0500

    updated project info for clojars

--- project.clj
+++ project.clj
@@ -1,9 +1,17 @@
-(defproject gengoclj "0.1.0-SNAPSHOT"
+(defproject gengoclj "0.1.0"
   :description "Gengo API for Clojure"
   :url "https://github.com/thoersch/gengo-clj"
+  :scm {:name "git"
+        :url "https://github.com/thoersch/gengo-clj"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.1.2"]
                  [clj-http "0.3.5"]]
+  :signing {:gpg-key "375B1908"}
+  :pom-addition [:developers [:developer
+                               [:name "Tyler Hoersch"]
+                               [:url "http://tylerhoersch.com"]
+                               [:email "thoersch@gmail.com"]
+                               [:timezone "-5"]]]
   :plugins [[lein-marginalia "0.8.0"]])
